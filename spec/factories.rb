d7065076164f3eabc4aee4c7288d2e5803e489c4
Slater-Victoroff <slater.victoroff@students.olin.edu>
2013-01-12 00:25:18 -0500

    Finished implementing edit, delete, index, destroy, and admin

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name	"Slater Victoroff"
-		email	"Slater.Victoroff@students.olin.edu"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email){ |n| "person_#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
+		
+		factory :admin do
+			admin true
+		end
 	end
 end
